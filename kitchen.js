dc8be6698aa13e849305125946aa6c49facce5a3
matt <matthew.eberhardt@gmail.com>
2020-04-11 09:17:47 -0500

    moved express to http instead of https. https is handled by reverse proxy

--- kitchen.js
+++ kitchen.js
@@ -3,16 +3,20 @@ const cors = require('cors');
 const app = express();
 const path = require('path');
 const fs = require('fs');
-const https = require('https');
+//const https = require('https');
+const http = require('http');
 
 app.use(cors());
 
+/*
 const sslOptions = {
     key: fs.readFileSync("/etc/letsencrypt/live/eberhardt.cloud/privkey.pem"),
     cert: fs.readFileSync("/etc/letsencrypt/live/eberhardt.cloud/fullchain.pem")
 };
+*/
 
-https.createServer(sslOptions, app).listen(2000);
+//https.createServer(sslOptions, app).listen(2000);
+http.createServer(app).listen(2000);
 
 //send all traffic/file requests to the react app in the build folder
 app.get('*', (request,response) =>{
