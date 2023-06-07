9a07c11840fc6ea520592c73d505b2daf09ce919
Ubuntu <matte@fastmail.com>
2023-06-07 15:04:41 -0500

    refactoring, serving both react app as well as backend through same express setup

--- kitchen.js
+++ kitchen.js
@@ -6,9 +6,22 @@ const fs = require('fs');
 const https = require('https');
 const http = require('http');
 const ip = require('my-local-ip')();
+const bookmark = require('./api/routes/bookmark');
+const tags = require('./api/routes/tags');
+const recipe = require('./api/routes/recipe');
+const search = require('./api/routes/search');
+const camera = require('./api/routes/camera');
+const upload = require('./api/routes/upload');
+const tesseract = require('node-tesseract');
+const multer = require('multer');
+const creds = JSON.parse(fs.readFileSync(__dirname+'/../kitchenapi.json', 'UTF-8'));
+const allowed_ip = ip.replace(/(?:\.\d+){1}$/, '');     //only allow devices on the same network (local) to make updates
+const uploadMulter = multer({ dest: __dirname+'/uploads/' });
 
 app.use(cors());
 
+//this isn't  needed as it's now behind a reverse proxy, the ssl certs are handled by it
+/*
 if(!ip.includes("10.192.106")){
 	const sslOptions = {
 		key: fs.readFileSync("/etc/letsencrypt/live/eberhardt.cloud/privkey.pem"),
@@ -23,8 +36,10 @@ if(!ip.includes("10.192.106")){
 	}).listen(2002);	
 }
 else{
-	http.createServer(app).listen(2002);		
+	http.createServer(app).listen(4100);		
 }
+*/
+http.createServer(app).listen(4100);		
 
 //send all traffic/file requests to the react app in the build folder
 app.get('*', (request,response) =>{
@@ -35,6 +50,20 @@ app.get('*', (request,response) =>{
 	else if(request.url.includes('acme-challenge')){
 		//this is for certbot for lets encrypt
 		response.sendFile(path.join(__dirname+'/'+request.url));
+	}
+	else if(request.url.includes('/api')){
+		//node-sqlite3 runs asynchronous, which leads to promise mess of chaining. better-sqlite3 runs synchronously
+		const DB_PATH = __dirname+'/sqlite.db';
+		const sqlite3 = require('better-sqlite3');
+		const DB = new sqlite3(DB_PATH);
+		//const DB = new sqlite3(DB_PATH, { verbose: console.log });
+
+		bookmark(app,DB,allowed_ip);
+		tags(app,DB,allowed_ip);
+		recipe(app,DB,creds,allowed_ip);
+		search(app,DB);
+		camera(app,DB,fs,tesseract,allowed_ip);
+		upload(app,DB,fs,tesseract,uploadMulter,allowed_ip);
 	}
 	else{
 		fs.access(__dirname+'/www/'+request.url, fs.F_OK, (err) => {
